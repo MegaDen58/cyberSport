702042b0ac13e6b6ece402f43e35e7a3db7aaa24
Denis <samparizona2018@yandex.ru>
2022-03-16 23:32:39 +0300

    Project added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
@@ -14,35 +15,89 @@ import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    int naviCounter = 0;
+    int g2Counter = 0;
+
+    Button plusPointToNavi;
+    Button plusPointToG2;
+    TextView naviScore;
+    TextView g2Score;
+    Button reset;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        plusPointToNavi = (Button) findViewById(R.id.plusPointToNavi);
+        naviScore = (TextView) findViewById(R.id.naviScore);
+        plusPointToG2 = (Button) findViewById(R.id.plusPointToG2);
+        g2Score = (TextView) findViewById(R.id.g2Score);
+        reset = (Button) findViewById(R.id.reset);
+        plusPointNavi();
+        plusPointG2();
+        Reset();
+        if (savedInstanceState != null && savedInstanceState.containsKey("naviCounter"))
+        {
+            naviCounter = savedInstanceState.getInt("naviCounter");
+        }
+        if (savedInstanceState != null && savedInstanceState.containsKey("g2Counter"))
+        {
+            g2Counter = savedInstanceState.getInt("g2Counter");
+        }
+        naviScore.setText(naviCounter + "");
+        g2Score.setText(g2Counter + "");
 
-        Button red = (Button) findViewById(R.id.red);
-        Button yellow = (Button) findViewById(R.id.yellow);
-        Button green = (Button) findViewById(R.id.green);
-        final LinearLayout layout = (LinearLayout) findViewById(R.id.LinearLayout);
+    }
 
-        red.setOnClickListener(new View.OnClickListener(){
+
+    void plusPointNavi() {
+        plusPointToNavi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                layout.setBackgroundColor(Color.RED);
+                naviCounter++;
+                naviScore.setText(naviCounter + "");
             }
         });
-
-        yellow.setOnClickListener(new View.OnClickListener(){
+    }
+    void plusPointG2() {
+        plusPointToG2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                layout.setBackgroundColor(Color.YELLOW);
+                g2Counter++;
+                g2Score.setText(g2Counter + "");
             }
         });
-
-        green.setOnClickListener(new View.OnClickListener(){
+    }
+    void Reset(){
+        reset.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                layout.setBackgroundColor(Color.GREEN);
+            public void onClick(View view){
+                naviScore.setText("0");
+                g2Score.setText("0");
+                naviCounter = 0;
+                g2Counter = 0;
             }
         });
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("naviCounter", naviCounter);
+        savedInstanceState.putInt("g2Counter", g2Counter);
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null &&
+                savedInstanceState.containsKey("naviCounter")) {
+            naviCounter = savedInstanceState.getInt("naviCounter");
+        }
+        if (savedInstanceState != null &&
+                savedInstanceState.containsKey("g2Counter")) {
+            naviCounter = savedInstanceState.getInt("g2Counter");
+        }
     }
 }
